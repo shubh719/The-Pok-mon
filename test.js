5fdd0a7b182dfb9fdb1550254f440df71833c3f2
shubh <shubhpatel.al@gmail.com>
2025-04-05 11:11:15 +0530

    add responsivness

--- test.js
+++ test.js
@@ -10,6 +10,14 @@ function render() {
       // Clear previous results
       document.querySelector(".result").innerHTML = "";
 
+      // Check if requested number exceeds available Pokemon
+      if (number.value > data.pokemon.length) {
+        alert(
+          `Error: There are only ${data.pokemon.length} ${selectedValue} type Pokemon available. Please enter a number less than or equal to ${data.pokemon.length}.`
+        );
+        return;
+      }
+
       // Loop through the requested number of Pokémon
       for (let i = 0; i < number.value; i++) {
         const allPokemon = data.pokemon;
@@ -29,11 +37,9 @@ function render() {
             pn.textContent = pokemonData.name;
 
             let img = document.createElement("img");
-
             img.src = pokemonData.sprites.front_default;
             img.className = "pokemon-image";
-
-            div.append(pn);
+            img.height = div.append(pn);
             div.append(img);
             document.querySelector(".result").appendChild(div);
           });
